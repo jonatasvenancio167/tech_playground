dfc64fe88ecf45147899bae509950a6f91c27052
Jonatas Venancio <jonatas@homehub.com.br>
2026-01-18 09:16:14 -0300

    feat: add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb",
+      }
+    },
+  },
+  plugins: [],
+}
